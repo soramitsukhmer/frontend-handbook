36428d58ce23233ddf908ae29d99a15b8524aaf4
Socheat Sok <alex@socheat.net>
2021-11-07 22:16:53 +0700

    feat: add extra configure scripts

---
+++ Contents/scripts/vscode-configure.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+code --install-extension EditorConfig.EditorConfig
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension esbenp.prettier-vscode
+code --install-extension octref.vetur
+code --install-extension wix.vscode-import-cost
