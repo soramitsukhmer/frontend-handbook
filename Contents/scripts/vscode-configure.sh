e959b36ea37b9f1778d24df7f8e11f7ea9d4d996
Socheat Sok <alex@socheat.net>
2022-06-08 11:51:52 +0700

    feat(docs): remove Contents

--- Contents/scripts/vscode-configure.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-code --install-extension EditorConfig.EditorConfig
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension esbenp.prettier-vscode
-code --install-extension octref.vetur
-code --install-extension wix.vscode-import-cost
