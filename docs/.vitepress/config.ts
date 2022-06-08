e8d573ae0daefd0b2746701c3fe3e17d4e210652
Socheat Sok <alex@socheat.net>
2022-06-08 12:12:56 +0700

    chore(docs): set vitepress base path to "/frontend-handbook/"

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from'vitepress'
 import { nav } from './nav'
 
 export default defineConfig({
+  base: '/frontend-handbook/',
   title: 'Frontend Handbook',
   description: 'Just playing around.',
   themeConfig: {
     nav: nav()
   },
-  base: '/frontend-handbook/'
 })
