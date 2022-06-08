7b528668d59e47a21673f067019fe40beb495c2d
Socheat Sok <alex@socheat.net>
2022-06-08 13:24:17 +0700

    feat(docs): update root link text

--- docs/.vitepress/nav.ts
+++ docs/.vitepress/nav.ts
@@ -1,7 +1,7 @@
 import type { DefaultTheme } from 'vitepress'
 export function nav(): DefaultTheme.NavItem[] {
     return [
-        { text: 'Frontend Handbook', link: '/' },
+        { text: 'Home', link: '/' },
         { text: 'Getting Started', link: '/getting-started/' }
     ]
 }
