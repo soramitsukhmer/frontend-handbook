e5836c5a8950d7ee0d4efd29aa09f4b75c85deb9
Socheat Sok <alex@socheat.net>
2022-06-09 11:13:55 +0700

    feat(gh-pages): add CNAME for frontend.handbook.soramitsukhmer.com

--- .github/scripts/gh-pages.sh
+++ .github/scripts/gh-pages.sh
@@ -10,7 +10,7 @@ npm run docs:build
 cd docs/.vitepress/dist
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+echo 'frontend.handbook.soramitsukhmer.com' > CNAME
 
 git init
 git add -A
